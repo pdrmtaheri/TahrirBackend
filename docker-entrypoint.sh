0ab5e644672726dd6bedf33dad01c2da70ad5e43
Pedram Taheri <pdrm.taheri@gmail.com>
2019-06-28 17:40:41 +0430

    Revert a mistakenly commented line

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,11 +7,11 @@ until psql -h postgres -p 5432 -U postgres -c '\q'; do
 done
 echo "Connected to database."
 
-#python3 manage.py migrate --noinput
-#if [ $? -ne 0 ]; then
-#    echo "Migration failed." >&2
-#    exit 1
-#fi
+python3 manage.py migrate --noinput
+if [ $? -ne 0 ]; then
+    echo "Migration failed." >&2
+    exit 1
+fi
 
 exec gunicorn --env DJANGO_SETTINGS_MODULE=TahrirBackend.settings TahrirBackend.wsgi \
     --name tahrir-gunicorn \
